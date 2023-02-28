12ffadadf1f854a4ab3e8c83c84e3e810a7c85b7
rakesh rawat <rakesh.rawat@cynoteck.com>
2023-02-28 12:59:20 +0530

    setting response problem

--- app/src/main/java/com/example/petofy/Home_Fragment.kt
+++ app/src/main/java/com/example/petofy/Home_Fragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.example.petofy.databinding.ActivityDashBoardBinding
+import com.example.petofy.databinding.FragmentHomeBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,33 +17,34 @@ import retrofit2.Response
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+
 /**
  * A simple [Fragment] subclass.
  * Use the [Home_Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Home_Fragment : Fragment() {
+class Home_Fragment : Fragment(R.layout.fragment_home_) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var binding:FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        var myPets= view?.findViewById<TextView>(R.id.txt_myPets)
-        var appointment=view?.findViewById<TextView>(R.id.txt_appointment)
-        var staff=view?.findViewById<TextView>(R.id.txt_staff)
-        var record=view?.findViewById<TextView>(R.id.txt_record)
-        var b2b=view?.findViewById<TextView>(R.id.txt_b2b)
+
+    ): View{
+
+        binding= FragmentHomeBinding.inflate(inflater,container,false)
 
         UserDashBoardCountClient.dashBoardCountInstance.GetDashBoardCount(token).enqueue(object : Callback<UserDashBoardCountResponse?> {
             override fun onResponse(
@@ -51,11 +54,9 @@ class Home_Fragment : Fragment() {
 
                  if(response.body()!=null)
                  {
-
-
-                     myPets?.text= response.body()?.data?.numberOfPets.toString().trim()
-                     appointment?.text= response.body()?.data?.numberOfAppointments.toString().trim()
-                     staff?.text= response.body()?.data?.numberOfStaffs.toString().trim()
+                     binding.txtMyPets.text=response.body()?.data?.numberOfPets.toString()
+                     binding.txtAppointment.text= response.body()?.data?.numberOfAppointments.toString()
+                     binding.txtStaff.text= response.body()?.data?.numberOfStaffs.toString()
 
                  }
             }
@@ -64,26 +65,8 @@ class Home_Fragment : Fragment() {
                 Log.d("error", "onResponse: not runnning")
             }
         })
-        return inflater.inflate(R.layout.fragment_home_, container, false)
+        return binding.root//inflater.inflate(R.layout.fragment_home_, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home_Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Home_Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
